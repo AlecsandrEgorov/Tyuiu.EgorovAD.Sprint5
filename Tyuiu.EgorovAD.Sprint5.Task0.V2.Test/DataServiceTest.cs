f1fae89d44b27ee893e02a5149dca6f850e598c4
Tyuiu.EgorovAD@outlook.com <e.alexa07@mail.ru>
2025-11-25 19:36:22 +0300

    Add project files.

---
+++ Tyuiu.EgorovAD.Sprint5.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.EgorovAD.Sprint5.Task0.V2.Lib;
+namespace Tyuiu.EgorovAD.Sprint5.Task0.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            string path = @"C:\Users\Xeon\source\repos\Tyuiu.EgorovAD.Sprint5\Tyuiu.EgorovAD.Sprint5.Task0.V2\bin\Debug\net8.0\OutPutFileTask0.txt";
+
+            FileInfo file = new FileInfo(path);
+
+            bool filesInf = file.Exists;
+
+            bool wait = true;
+
+            Assert.AreEqual(wait, filesInf);
+        }
+    }
+}
---
+++ Tyuiu.EgorovAD.Sprint5.Task0.V2/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.EgorovAD.Sprint5.Task0.V2.Lib;
+namespace Tyuiu.EgorovAD.Sprint5.Task0.V2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+
+            DataService ds = new DataService();
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("y = " + x);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл " + res);
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
